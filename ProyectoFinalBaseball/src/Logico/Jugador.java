2324657dcc119c19c746ce43988be2c34edd6f11
arolioz <arolio301911@gmail.com>
2025-03-13 15:22:58 -0400

    Agregando clase jugador

---
+++ ProyectoFinalBaseball/src/Logico/Jugador.java
@@ -0,0 +1,37 @@
+package Logico;
+
+public class Jugador {
+	private String id;
+	private String nombre;
+	private String cedula;
+	private String direccion; 
+	private String sexo;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getId() {
+		return id;
+	}
+}
