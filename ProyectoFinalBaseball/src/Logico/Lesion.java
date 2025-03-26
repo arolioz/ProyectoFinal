cf71f39bc81b0f131323c44ddf0b82a0a672c469
elian <elian@DESKTOP-TV8RIPV>
2025-03-26 11:15:56 -0400

    Agregando los atributos de Lesion

--- ProyectoFinalBaseball/src/Logico/Lesion.java
+++ ProyectoFinalBaseball/src/Logico/Lesion.java
@@ -1,5 +1,88 @@
 package Logico;
 
-public class Lesion {
+import java.util.Date;
 
+public class Lesion {
+	private String id;
+	private String tipoLesion;
+	private Juego juego;
+	private Jugador jugador;
+	private int prorroga;
+	private Date fechaLesion;
+	private Date fechaReincorporacion;
+	private String descripcion;
+	
+	public Lesion(String id, String tipoLesion, Juego juego, Jugador jugador, int prorroga, Date fechaLesion,
+			Date fechaReincorporacion, String descripcion) {
+		super();
+		this.id = id;
+		this.tipoLesion = tipoLesion;
+		this.juego = juego;
+		this.jugador = jugador;
+		this.prorroga = prorroga;
+		this.fechaLesion = fechaLesion;
+		this.fechaReincorporacion = fechaReincorporacion;
+		this.descripcion = descripcion;
+	}
+
+	public String getTipoLesion() {
+		return tipoLesion;
+	}
+
+	public void setTipoLesion(String tipoLesion) {
+		this.tipoLesion = tipoLesion;
+	}
+
+	public Juego getJuego() {
+		return juego;
+	}
+
+	public void setJuego(Juego juego) {
+		this.juego = juego;
+	}
+
+	public Jugador getJugador() {
+		return jugador;
+	}
+
+	public void setJugador(Jugador jugador) {
+		this.jugador = jugador;
+	}
+
+	public int getProrroga() {
+		return prorroga;
+	}
+
+	public void setProrroga(int prorroga) {
+		this.prorroga = prorroga;
+	}
+
+	public Date getFechaLesion() {
+		return fechaLesion;
+	}
+
+	public void setFechaLesion(Date fechaLesion) {
+		this.fechaLesion = fechaLesion;
+	}
+
+	public Date getFechaReincorporacion() {
+		return fechaReincorporacion;
+	}
+
+	public void setFechaReincorporacion(Date fechaReincorporacion) {
+		this.fechaReincorporacion = fechaReincorporacion;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getId() {
+		return id;
+	}
+	
 }
